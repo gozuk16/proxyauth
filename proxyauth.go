72318bf8dcb20f71b4cfaf8e5bce4290d13a8e6d
GOZU Kenichiro <gozuk16@gmail.com>
2019-12-24 19:31:39 +0900

    Chrome-Driverのオプションを追加 エラー処理をもう少し記述

--- proxyauth.go
+++ proxyauth.go
@@ -11,7 +11,8 @@ func main() {
 	options := agouti.ChromeOptions(
 		"args", []string{
 			"--headless",
-			"-disable-gpu", // 暫定的に必要とのこと
+			"-disable-gpu",                // 暫定的に必要とのこと
+			"--ignore-certificate-errors", // 認証関係のエラー無視
 			"--no-sandbox",
 			"--disable-xss-auditor",
 		})
@@ -20,18 +21,31 @@ func main() {
 	defer driver.Stop()
 
 	if err := driver.Start(); err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
+		fmt.Fprintf(os.Stderr, "Start error: %s\n", err)
 		return
 	}
 
 	page, err := driver.NewPage()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
+		fmt.Fprintf(os.Stderr, "NewPage error: %s\n", err)
 		return
 	}
 
 	page.Navigate("https://xxx.xxx.xxx.xxx:99999/proxy?foo=bar&hogehoge=http://neverssl.com")
-	fmt.Println(page.Title())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Navigate error: %s\n", err)
+		return
+	}
+
+	// Navigateは成功しているのにページがロードできてない？後の認証も失敗するので待ってみる
+	page.Session().SetImplicitWait(10000)
+
+	t, err := page.Title()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Title error: %s\n", err)
+		return
+	}
+	fmt.Println("Title: " + t)
 
 	fmt.Println("set userid & password")
 	page.FindByID("user").Fill("xxxxxxxx")
